4e615e0c4ff5a437c7a9b9aa9bd467aecc2daeac
Yukinari Tani <yukinarit84@gmail.com>
2019-05-24 12:03:01 +0900

    Add distutils-strtobool package

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # strtobool
+
 distutils.utils.strtobool. Because latest virtualenv doesn't ship distutils anymore.
+
+```python
+from strtobool import strtobool
+print(bool(strtobool('True')))
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='distutils-strtobool',
+    version='0.1.0',
+    description='distutils.utils.strtobool',
+    author='yukinarit',
+    author_email='yukinarit84@gmail.com',
+    url='https://github.com/yukinarit/strtobool',
+    license='Python Software Foundation License',
+    py_modules=['strtobool'],
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Framework :: Robot Framework :: Library',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
